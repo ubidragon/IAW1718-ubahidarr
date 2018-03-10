3d2717ca104c38fe4a5cb5dc0d76af48c5e1dfe4
ubidragon <ubidragon@protonmail.com>
2018-03-10 19:32:22 +0100

    Add more operations

--- pec1/operations.php
+++ pec1/operations.php
@@ -38,6 +38,10 @@ function selectModel($Marca){
 }
 function selectAge($age){
 	global $mysql;
+	/*Ejemplo de consulta*/
+	SELECT ID,Modelo from vehiculos where TIMESTAMPDIFF(YEAR, Fecha_Compra, NOW()) = 2 AND Marca='SEAT' 
+
+
 	$consulta = $mysql->query("SELECT  FROM marcamodelo WHERE Marca = $Marca ");
 	$text="";
  	$fila=$consulta->fetch_assoc();
@@ -51,7 +55,29 @@ function selectAge($age){
 	return $text;
 }
 function selectSales($precio){
-	$consulta="";
+	global $mysql;
+	$rango;
+	if($precio<1000){
+	$rango = "Precio < 1000";
+	}elseif ($precio>1000 && $precio<3000) {
+		$rango = "Precio > 1000 AND Precio < 3000";
+	}elseif ($precio>3000 && $precio<6000) {
+		$rango = "Precio > 3000 AND Precio < 6000";
+	}elseif ($precio>6000) {
+		$rango = "Precio > 6000";
+	}
+
+	$consulta = $mysql->query("SELECT ID FROM vehiculos WHERE $rango");
+	$text="";
+ 	$fila=$consulta->fetch_assoc();
+                while($fila){
+                    $text.= "<option value='{$fila["Precio"]}'>".
+                        "{$fila["Precio"]}</option>";
+                    $fila=$consulta->fetch_assoc();
+                }
+
+
+	return $text;
 }
 
 ?>
